c490461d54e5c510bf0dd6f89226b8f1ce1862f5
sanjeevbhandari <sanjeev.bhandari@vetted.work>
2026-02-03 08:43:44 +0545

    Add harper_ls grammar checker, enable inlay hints with toggle, and add keymap descriptions to LSP config

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,16 +1,11 @@
+local servers = { "lua_ls", "intelephense", "ts_ls", "vue_ls", "tailwindcss", "bashls", "harper_ls" }
+
 return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"intelephense",
-					"ts_ls",
-					"vue_ls",
-					"tailwindcss",
-					"bashls",
-				},
+				ensure_installed = servers,
 			})
 		end,
 	},
@@ -21,20 +16,48 @@ return {
 			"b0o/schemastore.nvim",
 			"saghen/blink.cmp",
 		},
-		config = function()
+		opts = {
+			servers = {
+				harper_ls = {
+					settings = {
+						harper = {
+							linters = {
+								sentence_capitalization = false,
+								spell_check = true,
+								repeated_words = true,
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			vim.lsp.config("*", { capabilities = capabilities })
 
-			vim.lsp.enable({ "lua_ls", "intelephense", "ts_ls", "vue_ls", "tailwindcss" })
+			for server, server_opts in pairs(opts.servers or {}) do
+				vim.lsp.config(server, server_opts)
+			end
 
-			vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>")
-			vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>")
-			vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>")
-			vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>")
+			vim.lsp.enable(servers)
 
-			vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover)
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+			vim.lsp.inlay_hint.enable(true)
+
+			-- Navigation
+			vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Go to definition" })
+			vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Go to type definition" })
+			vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Go to implementation" })
+			vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", { desc = "Go to references" })
+
+			-- Actions
+			vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Code action" })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+
+			-- Inlay hints toggle
+			vim.keymap.set("n", "<leader>uh", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+			end, { desc = "Toggle inlay hints" })
 		end,
 	},
 }
